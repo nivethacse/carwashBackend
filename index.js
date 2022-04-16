89ff1545f80ab07ee2ad54421ecf3d7241e6ff47
Nivetha s <akshaya2d@gmail.com>
2022-04-16 14:19:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const cors = require ("cors")
 const express = require ("express");
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 5000
 const db = require ("./mongoDB")
 
 const loginRoutes = require("./routes/user_routes")
